93253089119aa33a4d433dbc4e1894ffc380b90a
Ty Uehara <tyuehara@w10-8mnpsg2.ad.hawaiiantel.com>
2020-05-04 16:09:09 -1000

    Hawaiian Telcom UI

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import HTLogo from "../images/htlogo.png";
-import { GoPerson } from "react-icons/go";
+import CAFLogo from "../images/CAF.png";
 
 class NavBar extends React.Component {
   render() {
     return (
       <header className="header-flex">
-        <img className="htlogo" src={HTLogo} alt="Company Logo"></img>
-        <h3 style={{ paddingRight: "160px" }}>Connect America Fund Database</h3>
-        <p>
-          <GoPerson className="usericon" />
-        </p>
+        <img className="htlogo" src={HTLogo} alt="Company Logo" />
+        <h3 style={{ paddingRight: "100px", fontSize: "30px" }}>
+          Connect America Fund Database
+        </h3>
+        <img className="caflogo" src={CAFLogo} alt="Caf Logo" />
       </header>
     );
   }
